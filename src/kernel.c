b8e3b61417a5a851fa80f309ef2713d277a0c885
Hakob <tumanyan-hakob@mail.ru>
2025-06-21 11:48:53 +0400

    first push

--- src/kernel.c
+++ src/kernel.c
@@ -1,6 +1,6 @@
 int kernel_main () {
     char *video = (char*) 0xb8000;
-    const char *msg = "Hello from MyOS!";
+    const char *msg = "Hello from jacOS!";
     for (int i = 0; msg[i] != '\0'; ++i) {
         video[i * 2] = msg[i];
         video[i * 2 + 1] = 0x07;
